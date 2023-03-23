e1f2583236113df3be37b257a7d059790681eda8
Harry Rosmann <47433061+harryrosmann@users.noreply.github.com>
2023-03-23 14:59:10 -0400

    Added robustness to the communication system. Establishes and maintains connection through heartbeat signal. Responds instantaneously to change in deadman switch state.

--- Deadman_Switch_RX/Deadman_Switch_RX.ino
+++ Deadman_Switch_RX/Deadman_Switch_RX.ino
@@ -14,15 +14,21 @@
 /************ Radio Setup ***************/
 
 // Change to 434.0 or other frequency, must match RX's freq!
-#define RF69_FREQ       434.0   // 434 MHz
-#define CUTOFF_DELAY    100     //100ms cutoff delay (no message)
-#define GO              0x1     // Go status
-#define KILL            0x0     // Kill status
-#define UNKNOWN         0x2     // Unknown status
+#define RF69_FREQ 434.0         // 434 MHz
+#define HEARTBEAT_DELAY_MS 100  // Delay between heartbeat signals
+#define REPLY_TIMEOUT_MS   20   // Reply timeout before sending another message
+#define CONNECTION_TIMEOUT_MS 2000 // Number of ms to pass without reply
+                                   // before determining connection is lost
+#define GO 0x1                  // Go status
+#define STOP 0x0                // STOP status
+#define CONNECT 0x2             // Connection status
+#define UNKNOWN 0x3             // Unknown status
 
-#define GO_MESSAGE      "SEND IT"
-#define KILL_MESSAGE    "STOP"
-#define UNKNWON_MESSAGE "UNKNOWN"
+// Messages to send the car for each state
+#define CONNECTION_MSG "CONNECT"
+#define GO_MSG "SEND IT"
+#define STOP_MSG "STOP"
+#define UNKNOWN_MSG "UNKNOWN"
 
 #if defined (__AVR_ATmega32U4__) // Feather 32u4 w/Radio
   #define RFM69_CS      8
@@ -35,135 +41,245 @@
 // Singleton instance of the radio driver
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
-int CAR_STATUS = UNKNOWN;
+/** @brief Track ms between first transmission and reply */
+unsigned long reply_ms = 0UL;
 
-unsigned long last_go_ms = 0;
+/** @brief Tracks ms between each transmission signal */
+unsigned long heartbeat_ms = 0UL;
 
-void setup() 
-{
-  Serial.begin(115200);
-  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+/** @brief Car state, initialize to unkown */
+short car_state = UNKNOWN;
 
-  pinMode(LED, OUTPUT);     
-  pinMode(RFM69_RST, OUTPUT);
-  digitalWrite(RFM69_RST, LOW);
+/** @brief Current connection status */
+bool connected = false;
 
-  Serial.println("Feather RFM69 RX Test!");
-  Serial.println();
-
-  // manual reset
+/**
+ * @brief Initializes the RFM69 module
+ *
+ * @return True if initialization successful, otherwise false
+ */
+bool setup_RFM69() {
+  // Manual reset
   digitalWrite(RFM69_RST, HIGH);
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
-  
+
+  // Initialize RF Module
   if (!rf69.init()) {
     Serial.println("RFM69 radio init failed");
-    while (1);
+    blink(LED, 100, 3);
+    return false;
   }
+  blink(LED, 1000, 1); // Indicates power on
   Serial.println("RFM69 radio init OK!");
-  
+
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
   // No encryption
   if (!rf69.setFrequency(RF69_FREQ)) {
     Serial.println("setFrequency failed");
+    return false;
   }
 
   // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
   // ishighpowermodule flag set like this:
-  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+  rf69.setTxPower(20, true); // range from 14-20 for power, 2nd arg must be true for 69HCW
 
   // The encryption key has to be the same as the one in the server
-  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+  uint8_t key[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                   0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
   rf69.setEncryptionKey(key);
-  
-  pinMode(LED, OUTPUT);
-  pinMode(ON_OFF, OUTPUT);
 
-  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+  return true;
 }
 
-int Send_Reply(const char *message) {
-  rf69.send(message, sizeof(message));
+/**
+ * @brief Sets up  the digital inputs and outputs
+ */
+void setup_digital() {
+  pinMode(ON_OFF, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  
+  setup_digital();
+
+  Serial.println("Feather RFM69 RX Test!");
+  Serial.println();
+
+  if (!setup_RFM69()) {
+    Serial.println("Feather RFM69 initialization failed. Trying again...");
+  }
+  Serial.println("Feather RFM69 initialization successful!");
+  
+  Serial.print("RFM69 radio @");
+  Serial.print((int)RF69_FREQ);
+  Serial.println(" MHz");
+}
+
+/** 
+ * @brief Sets the connection flag, toggles led
+ */
+bool set_connection(bool is_connected) {
+  if (!is_connected) {
+    // Turn off the motor
+    toggle_power(false);
+  }
+
+  connected = is_connected;
+  toggle_led(is_connected);
+}
+
+/**
+ * @brief Takes in a message type and returns the string associated with 
+ * that message
+ * 
+ * @param[in] msg_type The type of message
+ * @return The string value associated with that message
+*/
+char *get_message_base(byte msg_type) {
+  switch (msg_type) {
+    case (GO):
+      return GO_MSG;
+    case (STOP):
+      return STOP_MSG;
+    case (CONNECT):
+      return CONNECTION_MSG;
+    default:
+      return UNKNOWN_MSG;
+  }
+}
+
+/**
+ * @brief Sends a message reply to the transmitting module
+*/
+void send_reply(short msg_type) {
+  char *msg;
+  uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+  unsigned long msg_send_start_ms; // When we first send the message
+
+  // Get the message to send
+  msg = get_message_base(msg_type);
+
+  // Triple the message and place it into packet
+  size_t msg_len = strlen(msg);
+  size_t pckt_len = 3 * (msg_len + 1);
+  char pckt[msg_len];
+  size_t i;
+  for (i = 0; i < msg_len; i += msg_len) {
+    strncpy(pckt + i, msg, msg_len);
+  }
+
+  rf69.send((uint8_t *)pckt, strlen(pckt));
   rf69.waitPacketSent();
-  Serial.println("Sent a reply");
-  Blink(LED, 40, 3); //blink LED 3 times, 40ms between blinks
+  Serial.print("Sent a reply: "); Serial.println(pckt);
+}
+
+void process_message(short msg_type) {
+  // Send the reply (echo the message)
+  send_reply(msg_type);
+
+  // Set the connection
+  switch (msg_type) {
+    case GO:
+    case STOP:
+    case CONNECT:
+      set_connection(true);
+      break;
+    default:
+      set_connection(false);
+  }
+
+  // If change of state, update accordingly
+  if (msg_type != car_state) {
+    if (msg_type == GO) {
+      toggle_power(true);
+    } else {
+      toggle_power(false);
+    }
+  }
 }
 
 /**
 * @brief Reads the message and sends a reply (echoes the message)
-* @return the message status
+* @return the true if a valid message received, otherwise false
 */
-int Receive_Message() {
-  int status = UNKNOWN;
+bool receive_message() {
+  short msg_state = UNKNOWN;
 
-  if (rf69.available()) {
+  if (rf69.waitAvailableTimeout(CONNECTION_TIMEOUT_MS)) {
     // Should be a message for us now   
     uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
     if (rf69.recv(buf, &len)) {
       if (!len) return;
       buf[len] = 0;
-      Serial.print("Received [");
-      Serial.print(len);
-      Serial.print("]: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf69.lastRssi(), DEC);
 
-      if (strstr((char *)buf, GO_MESSAGE)) {
-        Send_Reply(GO_MESSAGE);
-        status = GO;
-      } else if (strstr((char *)buf, KILL_MESSAGE)) {
-        Send_Reply(KILL_MESSAGE);
-        status = KILL;
-      } else {
-        Send_Reply(UNKNWON_MESSAGE);
-        status = UNKNOWN;
+      Serial.print("Received message: ");
+      Serial.println((char *)buf);
+
+      if (strstr((char *)buf, GO_MSG)) {
+        msg_state = GO;
+      } else if (strstr((char *)buf, STOP_MSG)) {
+        msg_state = STOP;
+      } else if (strstr((char *)buf, CONNECTION_MSG)) {
+        msg_state = CONNECT;
       }
 
-    } else {
-      Serial.println("Receive failed");
+      process_message(msg_state);
+
+      return true;
     }
   }
-
-  return status;
+  // Disconnected, timed out
+  set_connection(false);
+  return false;
 }
-
 
 void loop() {
-  int status = Receive_Message();
-
-  if (status == GO) {
-    last_go_ms = millis(); // Get the millis of the last go
-  }
-  
-  if (status != CAR_STATUS) {
-    // Status change
-    if (CAR_STATUS == GO) {
-      if ((status == KILL) || (millis() - last_go_ms > CUTOFF_DELAY)) {
-        // Kill power
-        digitalWrite(ON_OFF, LOW);
-      }
-    } else if (CAR_STATUS == KILL) {
-      if (status == GO) {
-        digitalWrite(ON_OFF, HIGH);
-      }
-    }
-    
-  }
-
-  CAR_STATUS = status;
+  receive_message();
 }
 
+/**
+ * @brief Toggles the LED high or low
+ * 
+ * @param[in] on The desired state of the LED
+*/
+void toggle_led(bool on) {
+  byte state = on ? HIGH : LOW;
+  digitalWrite(LED, state);
+}
 
-void Blink(byte PIN, byte DELAY_MS, byte loops) {
-  for (byte i=0; i<loops; i++)  {
-    digitalWrite(PIN,HIGH);
+/**
+ * @brief Toggles the LED high or low
+ * 
+ * @param[in] on The desired state of the LED
+*/
+void toggle_power(bool on) {
+  short state = on ? HIGH : LOW;
+  digitalWrite(ON_OFF, state);
+}
+
+/**
+ * @brief Blinks output at defined at PIN LOOPS times with DELAY_MS in between.
+ *
+ * @param PIN The pin to blink
+ * @param DELAY_MS The delay in ms between blinks
+ * @param loops The number of loops between blinks
+ */
+void blink(byte PIN, byte DELAY_MS, byte loops) {
+  for (byte i = 0; i < loops; i++) {
+    digitalWrite(PIN, HIGH);
     delay(DELAY_MS);
-    digitalWrite(PIN,LOW);
-    delay(DELAY_MS);
+    digitalWrite(PIN, LOW);
+    if (i != loops - 1) {
+      delay(DELAY_MS);
+    }
   }
 }
--- Deadman_Switch_TX/Deadman_Switch_TX.ino
+++ Deadman_Switch_TX/Deadman_Switch_TX.ino
@@ -5,178 +5,357 @@
 // reliability, so you should only use RH_RF69  if you do not need the higher
 // level messaging abilities.
 // It is designed to work with the other example rf69_server.
-// Demonstrates the use of AES encryption, setting the frequency and modem 
+// Demonstrates the use of AES encryption, setting the frequency and modem
 // configuration
 
 #include <SPI.h>
 #include <RH_RF69.h>
+#include <string.h>
 
 /************ Radio Setup ***************/
 
 // Change to 434.0 or other frequency, must match RX's freq!
-#define RF69_FREQ       434.0   // 434 MHz
-#define DEBOUNCE_DELAY  20      // 20ms debounce delay
-#define GO              0x1     // Go status
-#define KILL            0x0     // Kill status
+#define RF69_FREQ 434.0         // 434 MHz
+#define DEBOUNCE_DELAY_MS 20    // 20ms debounce delay
+#define HEARTBEAT_DELAY_MS 100  // Delay between heartbeat signals
+#define REPLY_TIMEOUT_MS   20   // Reply timeout before sending another message
+#define CONNECTION_TIMEOUT_MS 2000 // Number of ms to pass without reply
+                                   // before determining connection is lost
+#define GO 0x1                  // Go status
+#define STOP 0x0                // STOP status
+#define CONNECT 0x2             // Connection status
+#define UNKNOWN 0x3             // Unknown status
 
-#define GO_MESSAGE      "SEND IT"
-#define KILL_MESSAGE    "STOP"
-#define UNKNWON_MESSAGE "UNKNOWN"
+// Messages to send the car for each state
+#define CONNECTION_MSG "CONNECT"
+#define GO_MSG "SEND IT"
+#define STOP_MSG "STOP"
+#define UNKNOWN_MSG "UNKNOWN"
 
-#if defined (__AVR_ATmega32U4__) // Feather 32u4 w/Radio
-  #define RFM69_CS      8
-  #define RFM69_INT     7
-  #define RFM69_RST     4
-  #define LED           13
-  #define BUTTON        12
+#if defined(__AVR_ATmega32U4__) // Feather 32u4 w/Radio
+#define RFM69_CS 8
+#define RFM69_INT 7
+#define RFM69_RST 4
+#define LED 13
+#define BUTTON 12
 #endif
 
 // Singleton instance of the radio driver
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
-unsigned long start_ms = 0;  // time counter when the system starts up
+/** @brief Track ms between first transmission and reply */
+unsigned long reply_ms = 0UL;
 
-int BUTTON_STATUS   = LOW; // start as unpressed
+/** @brief Heartbeat counter, increment every go transmission */
+unsigned long n_heartbeat = 0;
 
-void setup() 
-{
-  Serial.begin(115200);
-  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+/** @brief Tracks ms between each heartbeat signal */
+unsigned long heartbeat_ms = 0UL;
 
-  pinMode(LED, OUTPUT);     
-  pinMode(RFM69_RST, OUTPUT);
-  digitalWrite(RFM69_RST, LOW);
+/** @brief Active high button state */
+short button_state = LOW;
 
-  Serial.println("Feather RFM69 TX Test!");
-  Serial.println();
+/** @brief Car state, initialize to unkown */
+short car_state = UNKNOWN;
 
-  // manual reset
-  digitalWrite(RFM69_RST, HIGH);
-  delay(10);
-  digitalWrite(RFM69_RST, LOW);
-  delay(10);
-  
-  if (!rf69.init()) {
-    Serial.println("RFM69 radio init failed");
-    while (1);
-  }
-  Serial.println("RFM69 radio init OK!");
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
-  // No encryption
-  if (!rf69.setFrequency(RF69_FREQ)) {
-    Serial.println("setFrequency failed");
-  }
-
-  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
-  // ishighpowermodule flag set like this:
-  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
-
-  // The encryption key has to be the same as the one in the server
-  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-  rf69.setEncryptionKey(key);
-  
-  pinMode(LED, OUTPUT);
-  pinMode(BUTTON, INPUT);
-
-  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
-}
+/** @brief Current connection status */
+bool connected = false;
 
 /**
-* @brief Checks if the button is pressed and takes into account the time to debounce
-*
-* @return true if the button is pressed, otherwise false
-*/
-bool Button_Pressed() {
-  int button_state = digitalRead(BUTTON);
-  if (button_state == LOW) {
-    return false;
-  }
-
-  /* Wait for delay to allow button to debounce */
-  unsigned long press_start = millis();
-  while (millis() - press_start < DEBOUNCE_DELAY) {
-    /* Spin */
-  }
-
-  button_state = digitalRead(BUTTON);
-  return button_state == HIGH;
-}
-
-bool Send_Message(char* radiopacket) {
-  bool acknowledged = false; // tracks whether message was echoed or not
-
-  Serial.print("Sending "); Serial.println(radiopacket);
-  
-  // Send a message!
-  rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
-  rf69.waitPacketSent();
-
-  // Now wait for a reply
-  uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-
-  if (rf69.waitAvailableTimeout(500))  { 
-    // Should be a reply message for us now   
-    if (rf69.recv(buf, &len)) {
-      if (strstr((char*)buf, radiopacket)) {
-        acknowledged = true;
-        Serial.println("Go acknowledged");
-      } else {
-        Serial.println("RX did not understand");
-      }
-    } else {
-      Serial.println("Receive failed");
-    }
-  } else {
-    Serial.println("No reply");
-  }
-
-  if (acknowledged) {
-    Blink(LED, 50, 3); //blink LED 3 times, 50ms between blinks
-    return true;
-  }
-  Blink(LED, 150, 1); //blink LED once
-  return false;
-}
-
-void loop() {
-  // Send the kill signal
-  if (!Button_Pressed()) {
-    // Reset variables
-    if (BUTTON_STATUS == HIGH) {
-      BUTTON_STATUS = LOW;
-      start_ms = 0;
-    }
-
-    if (!Send_Message(KILL_MESSAGE)) {
-      Serial.println("Kill signal not received");
-    }
-  } 
-  // Send the go signal
-  else {
-    if (BUTTON_STATUS == LOW) {
-      BUTTON_STATUS = HIGH;
-    }
-    
-    if (!Send_Message(GO_MESSAGE)) {
-      Serial.println("Go signal not received");
-    }
-
-    unsigned long duration = millis() - start_ms;
-    Serial.print("Duration: "); Serial.println(duration, DEC);
-  }
-
-}
-
-void Blink(byte PIN, byte DELAY_MS, byte loops) {
-  for (byte i=0; i<loops; i++)  {
-    digitalWrite(PIN,HIGH);
+ * @brief Blinks output at defined at PIN LOOPS times with DELAY_MS in between.
+ *
+ * @param PIN The pin to blink
+ * @param DELAY_MS The delay in ms between blinks
+ * @param loops The number of loops between blinks
+ */
+void blink(byte PIN, byte DELAY_MS, byte loops) {
+  for (byte i = 0; i < loops; i++) {
+    digitalWrite(PIN, HIGH);
     delay(DELAY_MS);
-    digitalWrite(PIN,LOW);
+    digitalWrite(PIN, LOW);
     if (i != loops - 1) {
       delay(DELAY_MS);
     }
   }
+}
+
+/**
+ * @brief Toggles the LED high or low
+ * 
+ * @param[in] on The desired state of the LED
+*/
+void toggle_led(bool on) {
+  byte state = on ? HIGH : LOW;
+  digitalWrite(LED, state);
+}
+
+/**
+ * @brief Checks if the button is pressed and takes into account the time to debounce
+ *
+ * @return true if the button is pressed, otherwise false
+ */
+bool button_pressed() {
+  int button = digitalRead(BUTTON);
+  
+  if (button == LOW) {
+    return false;
+  } else if (car_state == GO) {
+    // If button already pressed, no need to debounce
+    return true;
+  }
+
+  /* Wait for delay to allow button to debounce */
+  unsigned long press_start_ms = millis();
+  while ((millis() - press_start_ms) < DEBOUNCE_DELAY_MS) {
+    /* Spin */
+  }
+
+  button = digitalRead(BUTTON);
+  return (button == HIGH);
+}
+
+/** 
+ * @brief Attempts to establish conection between the two 
+ */
+bool establish_connection() {
+  if (!send_message(CONNECT)) {
+    return;
+  }
+
+  connected = true;
+  car_state = UNKNOWN;
+  toggle_led(true);
+}
+
+/** 
+ * @brief Sets connected flag to disconnected, car state to unknown
+*/
+void disconnect() {
+  // Message sending unsuccessful
+  connected = false;
+  car_state = UNKNOWN;
+  toggle_led(false);
+  Serial.println("Stop signal not received");
+  Serial.println("Disconnecting...")
+}
+
+/**
+ * @brief Sets up  the digital inputs and outputs
+ */
+void setup_digital() {
+  pinMode(BUTTON, INPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+}
+
+/**
+ * @brief Initializes the RFM69 module
+ *
+ * @return True if initialization successful, otherwise false
+ */
+bool setup_RFM69() {
+  // Manual reset
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
+
+  // Initialize RF Module
+  if (!rf69.init()) {
+    Serial.println("RFM69 radio init failed");
+    blink(LED, 100, 3);
+    return false;
+  }
+  blink(LED, 1000, 1); // Indicates power on
+  Serial.println("RFM69 radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+  // No encryption
+  if (!rf69.setFrequency(RF69_FREQ)) {
+    Serial.println("setFrequency failed");
+    return false;
+  }
+
+  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+  // ishighpowermodule flag set like this:
+  rf69.setTxPower(20, true); // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+  // The encryption key has to be the same as the one in the server
+  uint8_t key[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                   0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+  rf69.setEncryptionKey(key);
+
+  return true;
+}
+
+/**
+ * @brief Initializes the RF transceiver, establishes the connection
+ * between the two RF modules. Also sets the encrpytion key.
+ *
+ * For debugging purposes, sets up the serial commication.
+ */
+void setup()
+{
+  Serial.begin(115200);
+
+  setup_digital();
+
+  Serial.println("Feather RFM69 TX Test!");
+  Serial.println();
+
+  while (!setup_RFM69()) {
+    Serial.println("Feather RFM69 initialization failed");
+  }
+  Serial.println("Feather RFM69 initialization successful!");
+
+  Serial.print("RFM69 radio @");
+  Serial.print((int)RF69_FREQ);
+  Serial.println(" MHz");
+}
+
+/**
+ * @brief Takes in a message type and returns the string associated with 
+ * that message
+ * 
+ * @param[in] msg_type The type of message
+ * @return The string value associated with that message
+*/
+char *get_message_base(byte msg_type) {
+  switch (msg_type) {
+    case (GO):
+      return GO_MSG;
+    case (STOP):
+      return STOP_MSG;
+    case (CONNECT):
+      return CONNECTION_MSG;
+    default:
+      return UNKNOWN_MSG;
+  }
+}
+
+/**
+ * @brief Sends a message to the receiver. Attempts to send a message 
+ * until it is received, or connection times out.
+ * 
+ * @param[in] msg_type The type of message to send
+ * @return True if message was received and validated, otherwise false
+*/
+bool send_message(short msg_type) {
+  char *msg;
+  uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+  unsigned long msg_send_start_ms; // When we first send the message
+
+  // Get the message to send
+  msg = get_message_base(msg_type);
+
+  // Triple the message and place it into packet
+  size_t msg_len = strlen(msg);
+  size_t pckt_len = 3 * (msg_len + 1);
+  char pckt[msg_len];
+  size_t i;
+  for (i = 0; i < msg_len; i += msg_len) {
+    strncpy(pckt + i, msg, msg_len);
+  }
+
+  msg_send_start_ms = millis();
+
+  // Keep sending the message until receives reply or connection is lost
+  while ((msg_send_start_ms - millis()) < CONNECTION_TIMEOUT_MS) {
+    // Send the message
+    Serial.print("Sending: "); Serial.println(pckt);
+    rf69.send((uint8_t *)pckt, strlen(msg_len));
+    rf69.waitPacketSent();
+    
+    // Wait for reply
+    if (rf69.waitAvailableTimeout(REPLY_TIMEOUT_MS)) {
+      // Should be a reply message for us now
+      if (rf69.recv(buf, &len)) {
+        if (strstr((char *)buf, msg)) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
+/**
+ * @brief Sends the first message to the car during a state change.
+ * This message runs asynchronously, and is not dependent on the 
+ * heartbeat.
+ * 
+ * @param[in] next_car_state The new desired car state
+*/
+void send_init_message(short next_car_state) {
+  if (car_state == next_car_state) {
+    // Car state is not different
+    // Should be sending heartbeat
+    return;
+  }
+
+  if (!send_message(next_car_state)) {
+    disconnect();
+    return;
+  }
+
+  // Message received, update car state
+  car_state = next_car_state;
+
+  // Reset heartbeat state
+  heartbeat_ms = millis();
+  n_heartbeat = 0;
+}
+
+/**
+ * @brief Sends a hearbeat signal with the current state of the car
+*/
+void send_heartbeat() {
+  // Not time to send a heartbeat, continue
+  if ((heartbeat_ms - millis()) < HEARTBEAT_DELAY_MS) {
+    return;
+  }
+
+  // Send heartbeat
+  if (!send_message(car_state)) {
+    disconnect();
+  }
+
+  Serial.println("Heartbeat %lu received", n_heartbeat);
+
+  // Update heartbeat counters
+  n_heartbeat++;
+  heartbeat_ms = millis();
+}
+
+/**
+ * @brief The continous loop. If not connected, tries to establish connection.
+ * Sends an intitial message for any state change in the button. Otherwise
+ * sends heartbeat with the current car state to maintain connection.
+*/
+void loop()
+{
+  // Try to establish connection if not already established
+  if (!connected) {
+    if (!establish_connection()) {
+      return;
+    }
+  }
+
+  // Send the stop signal
+  if (!button_pressed()) {
+    // If button state already low, just continue
+    if (car_state != STOP) {
+      send_init_message(STOP);
+    }
+  } else {
+    if (car_state != GO) {
+      send_init_message(GO);
+    }
+  }
+
+  // Continue sending the heartbeat signal for current state
+  send_heartbeat();
 }
